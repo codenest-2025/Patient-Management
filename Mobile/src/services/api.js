cca86ada95d82535a573a044a43560eb10fe32bd
codenest-2025 <santampatra12345@gmail.com>
2026-05-06 23:59:26 +0530

    Update

--- Mobile/src/services/api.js
+++ Mobile/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 // Replace with your local machine IP for Android emulator
-const API_URL = "http://10.179.98.170:5000/api"; 
+const API_URL = process.env.EXPO_PUBLIC_API_URL || "http://10.179.98.170:5000/api"; 
 
 const api = axios.create({
   baseURL: API_URL,
